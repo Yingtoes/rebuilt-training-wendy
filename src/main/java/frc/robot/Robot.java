60999da8dc2fba4b15312d1af3be7e9e3b72d3a0
Yingtoes <Wendyytzhao@gmail.com>
2026-08-07 20:47:19 -0700

    feat: spindexer

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -5,8 +5,15 @@
 package frc.robot;
 
 import edu.wpi.first.wpilibj.TimedRobot;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 
 public class Robot extends TimedRobot {
+
+    private final SpindexSubsystem spindexer = new SpindexSubsystem();
+
+    public Robot() {
+        SmartDashboard.putData("Spindexer", spindexer);
+    }
 
     @Override
     public void robotInit() {}
